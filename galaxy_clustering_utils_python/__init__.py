5b2871591f426490a685cf0758808cd20a490c88
aszewciw <aszewci@gmail.com>
2017-08-25 15:27:27 -0500

    forgot to fix init

--- galaxy_clustering_utils_python/__init__.py
+++ galaxy_clustering_utils_python/__init__.py
@@ -5,7 +5,8 @@ __all__ = ["get_path"]
 from .get_path import get_system
 from .get_path import get_base_path
 from .get_path import get_scripts_path
-from .get_path import get_results_path
+from .get_path import get_adam_path
+from .get_path import get_gillian_path
 from .get_path import get_utils_path
 from .get_path import get_carmen_path
 from .get_path import get_carmenHD_path
