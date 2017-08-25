01dc290ea85ddaab6488ab75447db16a943471d6
aszewciw <aszewci@gmail.com>
2017-08-25 15:26:01 -0500

    more custom utils updates for stampede

--- galaxy_clustering_utils_python/get_path.py
+++ galaxy_clustering_utils_python/get_path.py
@@ -10,7 +10,7 @@ Description:
 #Setup file paths for common systems I work on.
 
 
-__all__        =['get_system','get_base_path','get_scripts_path','get_results_path','get_rawdata_path','get_cleandata_path']
+__all__        =['get_system','get_base_path','get_scripts_path','get_adam_path','get_gillian_path','get_rawdata_path','get_cleandata_path']
 __copyright__  =["Copyright 2016 Victor Calderon, common utilities in python; modified by Adam Szewciw"]
 __author__     =['Adam Szewciw']
 __email__      =['adam.o.szewciw@vanderbilt.edu']
@@ -29,7 +29,7 @@ def get_system():
     """
 
     import os, sys
-    path_to_home = os.getenv("HOME")
+    # path_to_home = os.getenv("HOME")
 
     host = os.popen('echo $HOSTNAME').read()
     tmp = host.split('.')[0]
@@ -43,13 +43,6 @@ def get_system():
 
     if host in known_systems(): return host
     else:
-        # host = None
-        # if os.path.isfile(os.getenv("HOME")+'/.bender'):
-        #     host = 'bender'
-        # elif os.path.isfile(os.getenv("HOME")+'/.victor-mac-pro'):
-        #     host = "Adam's-MacBook-Pro-2"
-        # else: raise ValueError('unknown system.')
-        # return host
         raise ValueError('unknown system.')
 
 
@@ -61,9 +54,11 @@ def get_base_path(node=None):
     if node==None: node = get_system()
 
     if node=='bender':
-        path = '/fs1/szewciw/galaxy_clustering/'
+        path = '/fs1/szewciw/galaxy_clustering'
     elif node=='Adams-MacBook-Pro-2':
-        path = '/Users/Adam/Codes/SEGUE/galaxy_clustering/'
+        path = '/Users/Adam/Codes/galaxy_clustering'
+    elif node=='stampede' or node=='stampede2':
+        path = os.getenv('HOME') + '/galaxy_clustering'
     else:
         raise ValueError('error: unknown data directory for this enviornment!')
 
@@ -76,27 +71,39 @@ def get_scripts_path(node=None):
     """
     if node==None: node = get_system()
 
-    if node!='bender' and node!='Adams-MacBook-Pro-2':
+    if not (node in known_systems()):
         raise ValueError('error: unknown code directory for this environment')
 
-    path = get_base_path(node)+'codes/'
+    path = get_base_path(node)+'/codes'
 
     return path
 
 
-def get_results_path(node=None):
+def get_adam_path(node=None):
     """
-    get the base path to results folder
+    get the base path to Adam's results folder
     """
     if node==None: node = get_system()
 
-    if node!='bender' and node!='Adams-MacBook-Pro-2':
+    if not (node in known_systems()):
         raise ValueError('error: unknown code directory for this environment')
 
-    path = get_base_path(node)+'results/'
+    path = get_base_path(node)+'/adam'
 
     return path
 
+def get_gillian_path(node=None):
+    """
+    get the base path to Gillian's results folder
+    """
+    if node==None: node = get_system()
+
+    if not (node in known_systems()):
+        raise ValueError('error: unknown code directory for this environment')
+
+    path = get_base_path(node)+'/gillian'
+
+    return path
 
 def get_utils_path(node=None):
     """
@@ -104,10 +111,10 @@ def get_utils_path(node=None):
     """
     if node==None: node = get_system()
 
-    if node!='bender' and node!='Adams-MacBook-Pro-2':
+    if not (node in known_systems()):
         raise ValueError('error: unknown code directory for this environment')
 
-    path = get_base_path(node)+'utils/'
+    path = get_base_path(node)+'/utils'
 
     return path
 
@@ -121,7 +128,7 @@ def get_carmen_path(node=None):
     if node!='bender':
         raise ValueError('error: data only exists on bender')
 
-    path = '/fs2/andreas/Carmen/Planck/'
+    path = '/fs2/andreas/Carmen/Planck'
 
     return path
 
@@ -135,21 +142,21 @@ def get_carmenHD_path(node=None):
     if node!='bender':
         raise ValueError('error: data only exists on bender')
 
-    path = '/fs2/andreas/Carmelota/Planck/'
+    path = '/fs2/andreas/Carmelota/Planck'
 
     return path
 
 
 def get_corrfunc_path(node=None):
     """
-    get the base path to folder containing results of carmenHD simulations
+    get the base path to folder containing manodeep's corrfunc
     """
     if node==None: node = get_system()
 
     if node!='bender':
         raise ValueError('error: just run corrfunc on bender')
 
-    path = '/fs1/szewciw/Corrfunc/'
+    path = '/fs1/szewciw/Corrfunc'
 
     return path
 
@@ -161,7 +168,7 @@ def get_pix_file(node=None):
     if node==None: node = get_system()
 
     if node!='bender':
-        raise ValueError('error: just run corrfunc on bender')
+        raise ValueError('Pixfile on bender')
 
     file = '/home/piscioja/SDSSPix/Maps/lss_geometry_north.dr72.pix'
 
